cd718999ebf2c11180e1464cbfe51be290ee4bca
GitHub Action <action@github.com>
2022-12-07 15:20:45 +0800

    Update portable.h

--- src/portable.h
+++ src/portable.h
@@ -102,6 +102,8 @@ std::wstring GetCommand(LPWSTR param)
 
             args.push_back(L"--disable-features=RendererCodeIntegrity,FlashDeprecationWarning");
             args.push_back(L"--enable-features=PlatformHEVCDecoderSupport");
+            args.push_back(L"--disable-machine-id");
+            args.push_back(L"--disable-encryption-win");
 
             // if (IsNeedPortable())
             //{
