26c367c8febf176bc60ffaf6e2ec33324661a85e
GitHub Action <action@github.com>
2023-01-27 18:07:06 +0800

    Update portable.h

--- src/portable.h
+++ src/portable.h
@@ -100,7 +100,9 @@ std::wstring GetCommand(LPWSTR param)
             // args.push_back(L"--force-local-ntp");
             // args.push_back(L"--disable-background-networking");
 
-            args.push_back(L"--disable-crash-reporter --disable-breakpad --no-report-upload --disable-logging --realtime-reporting-url=0.0.0.0 --reporting-connector-url=0.0.0.0 --blink-settings=\"dnsPrefetchingEnabled=false\"");
+            args.push_back(L"--disable-crash-reporter --disable-breakpad --no-report-upload --disable-logging");
+            args.push_back(L"--realtime-reporting-url=0.0.0.0 --reporting-connector-url=0.0.0.0");
+            args.push_back(L"--blink-settings="dnsPrefetchingEnabled=false"");
             args.push_back(L"--disable-features=RendererCodeIntegrity,FlashDeprecationWarning,CrashReporting,CrostiniAdditionalEnterpriseReporting,DocumentReporting,Reporting");
             args.push_back(L"--enable-features=PlatformHEVCDecoderSupport,EnableTabMuting");
             // args.push_back(L"--disable-machine-id");
